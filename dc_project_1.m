b042306fc967f1b457147e9ac7429c365e7f29d8
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2022-04-24 19:50:17 +0530

    Update dc_project_1.m

--- dc_project_1.m
+++ dc_project_1.m
@@ -4,20 +4,35 @@ clear;
 close all;
 clc;
 
+% Parameters
 rb = 100; % bitrate in hertz
 tb = 1/rb;
 tmax = 10*tb;
 tr = 0.001;
 alpha = 0;
-t = [-0.05 : tr : 0.05];
+t = -0.05 : tr : 0.05;
 
-p(t>=tmax & t<=tmax) = (sinc(rb.*t).*cos(pi.*alpha.*rb.*t))/1-(4.*(alpha^2).*(rb^2).*(t^2));
+% Raised Cosine Pulse
+p = (sinc(rb.*t).*cos(pi.*alpha.*rb.*t))/1-(4.*alpha.^2.*rb.^2.*t.^2);
 
-pf = laplace(p);
+% Frequency Spectrum
+nfft = length(t);
+nfft = 2^ceil(log2(nfft));
+f = ((-nfft/2):(nfft/2)-1)/(nfft*tb);
+f1 = fft(p,nfft);
+f1 = fftshift(f1);
 
+% Plotting spectrum
 figure(1);
-subplot(3,2,1);
+sgtitle("Pulse Shaping with Nyquist Pulses");
+subplot(2,1,1);
 plot(t,p);
 title("Raised cosine spectrum");
-xlabel("t");
-ylabel("T");
+xlabel("Time");
+ylabel("Amplitude");
+
+subplot(2,1,2);
+plot(f,f1);
+title("Frequency spectrum");
+xlabel("Frequency");
+ylabel("Amplitude");
