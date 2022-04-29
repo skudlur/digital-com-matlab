f997d4a1314d299c8473538232229567a817141a
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2022-04-29 22:27:29 +0530

    Update dc_project_2.m

--- dc_project_2.m
+++ dc_project_2.m
@@ -1,8 +1,14 @@
 %% Bit error rate for BPSK
 
 eb = 1;
-n = 10000000;
+n = 1000;
 ebno = -4:2:10;
+
+nv = (2.*ebno).^-1;
+wn = wgn(n,1,0);
+
+figure(1);
+plot(wn);
 
 d = randi([0 1],n,1,'int8');
 out = nrSymbolModulate(d, 'BPSK');
