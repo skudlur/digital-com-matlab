84a40671e0ce83de19c7bf16516a7bcc82684c13
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2022-05-03 22:11:31 +0530

    Update dc_project_2.m

--- dc_project_2.m
+++ dc_project_2.m
@@ -8,7 +8,14 @@ nv = (2.*ebno).^-1;
 wn = wgn(n,1,0);
 
 figure(1);
+subplot(3,1,1);
 plot(wn);
 
 sn = randi([-1 1],n,1);
 out = nrSymbolModulate(sn, 'BPSK');
+
+subplot(3,1,2);
+plot(sn);
+
+subplot(3,1,3);
+plot(out);
